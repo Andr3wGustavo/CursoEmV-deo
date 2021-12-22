abaf8896899687d0c07a482fbbb5084f0d4ee4a3
andrezin <andreadgustavo@gmail.com>
2021-12-22 01:38:48 -0300

    22/12

--- !!Exercícios/ex035.py
+++ !!Exercícios/ex035.py
@@ -1,13 +1,9 @@
 # Analisador de triangulo
-limpa = '\033[m'
-vermelho = '\033[31m'
-verde = '\033[32m'
-amarelo = '\033[33m'
-azul = '\033[34m'
+
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-print('{}-={}'.format(amarelo, limpa)*12)
-print('{}ANALISADOR DE TRIÂNGULO{}'.format(azul, limpa))
-print('{}-={}'.format(amarelo, limpa)*12)
+print('-='*12)
+print('ANALISADOR DE TRIÂNGULO')
+print('-='*12)
 a = float(input('Digite o primeiro segmento: '))
 b = float(input('Digite o segundo segmento: '))
 c = float(input('Digite o terceiro segmento: '))
@@ -15,7 +11,7 @@ c = float(input('Digite o terceiro segmento: '))
 if a < b + c and b < c + a and c < a + b:
     # Basicamente se a soma de dois lados forem menor que um dos lados, não pode se formar triagulo
     # Mas se a soma de dois segmentos forem maior que um segmento, pode se formar triangulo
-    print('{}O segmentos acima PODEM formar um triângulo!{}'.format(verde, limpa))
+    print('O segmentos acima PODEM formar um triângulo!')
 else:
-    print('{}Os segmentos acima NÃO PODEM formar um triângulo!{}'.format(vermelho, limpa))
+    print('Os segmentos acima NÃO PODEM formar um triângulo!')
 
--- !!Exercícios/ex050.py
+++ !!Exercícios/ex050.py
@@ -1,11 +1,11 @@
 # Soma de numeros pares
-print('Digite 6 números no terminal! ')
+print('Digite 6 numeros no terminal! ')
 soma = 0
 contador = 0
 for x in range(1, 7):
-    num = int(input('Digite o {}º número: '.format(x)))
+    num = int(input('Digite o {}º numero: '.format(x)))
 
     if num % 2 == 0:  # Se for par ele faz a soma
         soma += num  # Acumulador, a cada número par, ele soma
         contador += 1  # Conta quantos números pares foram digitados
-print('A soma entre os {} números pares  digitados  é {}.'.format(contador, soma))
+print('A soma entre os {} numeros pares  digitados  e {}.'.format(contador, soma))
