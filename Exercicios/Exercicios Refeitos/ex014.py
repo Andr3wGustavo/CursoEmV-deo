ef8442d7c781d594109255a2225e2e2605c27591
andrezin <andreadgustavo@gmail.com>
2021-12-22 23:07:56 -0300

    exercicios refeitos

---
+++ Exercicios/Exercicios Refeitos/ex014.py
@@ -0,0 +1,2 @@
+temperatura = float(input('Digite a temperatura em C°: '))
+print(f'{temperatura} C° é equivalente a {temperatura*1.8+32} °F ')
---
+++ Exercicios/Exercicios Refeitos/ex015.py
@@ -0,0 +1,3 @@
+quilometros = float(input('Digite quantos quilometros o carro percorreu: '))
+dias = int(input('Digite por quantos dias o carro foi alugado: '))
+print(f'O preço a pagar pelo aluguel do carro é de R${(dias*60)+(quilometros*0.15)}')
